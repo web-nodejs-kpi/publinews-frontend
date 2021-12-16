71b2c16a15e39e109bbbb5c44f1c20ffb7fb3641
Andrey Vlasov <andrey.vlasov02@gmail.com>
2021-12-16 18:54:44 +0200

    Access API via axios

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import Editor from './editor/Editor.js'
 import Feed from './feed/Feed.js'
 import Notes from './notes/Notes.js'
 import Sources from './sources/Sources.js'
-function App() {
-    return (
-        <div className="App">
-            <h1>Publinews</h1>
-            <Editor />
-            <Feed />
-            <Notes />
-            <Sources />
-        </div>
-    )
-}
+import React from 'react'
+import axios from 'axios'
 
-export default App
+const api = axios.create({
+    baseURL: `http://localhost:9000`,
+})
+
+export default class App extends React.Component {
+    constructor() {
+        super()
+        api.get('/').then(res => {
+            console.log(res.data)
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Publinews</h1>
+                <Editor />
+                <Feed />
+                <Notes />
+                <Sources />
+            </div>
+        )
+    }
+}
