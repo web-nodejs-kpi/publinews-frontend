8614bac3f2db433943366276958937b1a5e69a12
Andrey Vlasov <andrey.vlasov02@gmail.com>
2021-12-17 23:18:17 +0200

    Add formating for notes

--- src/sources/Source.js
+++ src/sources/Source.js
@@ -8,10 +8,19 @@ export default class Source extends React.Component {
     render() {
         return (
             <div className="Source">
-                Source: {JSON.stringify(this.props.info)}.
-                <button className="delete" onClick={this.onClickDelete}>
-                    &times;
-                </button>
+                <br />
+                <b>
+                    {this.props.info.name}{' '}
+                    <button className="delete" onClick={this.onClickDelete}>
+                        &times;
+                    </button>
+                </b>
+                <br />
+                Social network: {this.props.info.social_network_id}
+                <br />
+                Link: {this.props.info.link}
+                <br />
+                Rubric: <i>{this.props.info.rubric}</i>
             </div>
         )
     }
