89210b0093154b2ae696f40e0cc0a16ed3c0d5b1
Andrey Vlasov <andrey.vlasov02@gmail.com>
2021-12-17 23:50:29 +0200

    Edit formatting for post

--- src/feed/Post.js
+++ src/feed/Post.js
@@ -16,9 +16,7 @@ export default class Post extends React.Component {
                         Note
                     </button>
                 </b>
-                <br />
                 <p>{this.props.info.text}</p>
-                <br />
                 <a href={this.props.info.link}>original</a>
                 <small> {this.props.info.created_at}</small>
             </div>
